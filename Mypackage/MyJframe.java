44948689662f81c416e33911fcee0ac6176877df
Ujjwal <uj643570ujjwal@gmail.com>
2025-03-25 11:58:22 +0530

    a

---
+++ Mypackage/MyJframe.java
@@ -0,0 +1,27 @@
+package Mypackage;
+
+import java.util.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MyJframe {
+        public MyJframe() {
+                System.out.println("hello from MyJframe");
+                JFrame frame = new JFrame("Circuit Play");
+                JPanel panel = new JPanel();
+                JLabel label = new JLabel("My dream project");
+                ImageIcon img = new ImageIcon();
+                panel.add(label);
+                frame.add(panel);
+                frame.setIconImage(img.getImage());
+
+                frame.setSize(200, 300);
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        }
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+import javax.swing.JFrame;
+
+import Mypackage.MyJframe;
+
+public class main extends MyJframe {
+    public static void main(String[] args) {
+        new MyJframe();
+    }
+}
